c533a457bd8fadbdc8a6e12f363447cf5f546788
s-115-16 <s-115-16@gumiforest.ae.krakow.pl>
2019-10-21 14:05:14 +0200

    t2z12

---
+++ 02-ControlStructures/12.py
@@ -0,0 +1,7 @@
+x = int(input("Podaj x: "))
+y = int(input("Podaj y: "))
+
+if x < 0 or y < 0:
+    print("Co najmniej jedna ze zmiennych ma wartość ujemną")
+else:
+    print("Obie zmienne są dodatnie")
