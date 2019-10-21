39391756e9f3e9643eaccbdea4de6e516c8ba9f9
s-115-16 <s-115-16@gumiforest.ae.krakow.pl>
2019-10-21 14:05:00 +0200

    t2z11

---
+++ 02-ControlStructures/11.py
@@ -0,0 +1,7 @@
+login = input("Podaj login: ")
+password = input("Podaj hasło: ")
+
+if login == "marek" and password == "m-123":
+    print("Dane prawidłowe")
+else:
+    print("Dane nieprawidlowe")
