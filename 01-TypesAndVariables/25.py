26f7da8ae99b7a521948e5d3ee337a68355710b8
pat36 <pat36185@gmail.com>
2019-10-24 23:38:27 +0200

    t1z25

---
+++ 01-TypesAndVariables/25.py
@@ -0,0 +1,9 @@
+number = input("Podaj numer konta: ")
+print(number[0:2], end=" ")
+print(number[2:6], end= " ")
+print(number[6:10], end= " ")
+print(number[10:14], end= " ")
+print(number[14:18], end= " ")
+print(number[18:22], end= " ")
+print(number[22:26], end= " ")
+
